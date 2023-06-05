ace6c0b9039909bb2062fa2f5d71edbddf471025
GeneralYadoc <133023047+GeneralYadoc@users.noreply.github.com>
2023-06-06 01:16:24 +0900

    Remove unused package from requirements.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _requires_from_file(filename):
 
 setup(
     name="chatai-streamer",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     description="ChatGPT answer aloud YouTube chat messages.",
     author="General Yadoc",
--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .ChatAIStreamer import *
 from .GttsAIStreamer import *
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
